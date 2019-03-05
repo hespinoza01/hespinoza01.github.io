918caec4edccb18c5f33028fe0b3c1b39e0fcf85
Harold Espinoza <haroldesptru@gmail.com>
2019-03-04 22:40:00 -0600

    Añadida sección a mostrar en modo de notificación sobre la construcción del sitio en progreso, actua como un compenente el cual se carga al final del loader.

---
+++ js/building-site.js
@@ -0,0 +1,27 @@
+document.getElementById('main').innerHTML = `
+<section class="building-site">
+      <p class="building-site--title">EN<br>CONSTRUCCIÓN</p>
+      <p class="building-site--subtitle">EL SITIO ESTARÁ LISTO PRONTO</p>
+      <progress class="building-site--progress" max="100" value="10"></progress>
+      <div class="building-site--progress-indicator"><span>10%</span><span>100%</span></div>
+
+      <div class="building-site--social-icons">
+
+        <a href="https://twitter.com/@hespinoza__" target="_blank" title="Twitter" class="link-icon i-twitter"><svg class="icon"><use xlink:href="#icon-twitter"></use></svg></a>
+
+        <a href="https://www.linkedin.com/in/harold-espinoza-b64a9a115" target="_blank" title="Linkedin" class="link-icon i-linkedin"><svg class="icon"><use xlink:href="#icon-linkedin"></use></svg></a>
+
+        <a href="https://github.com/hespinoza01" target="_blank" title="Github" class="link-icon i-github"><svg class="icon"><use xlink:href="#icon-github"></use></svg></a>
+
+        <a href="https://www.instagram.com/hespinoza__" target="_blank" title="Instagram" class="link-icon i-instagram"><svg class="icon"><use xlink:href="#icon-instagram"></use></svg></a>
+
+        <a href="mailto:haroldesptru@gmail.com" target="_blank" title="haroldesptru@gmail.com" class="link-icon i-email"><svg class="icon"><use xlink:href="#icon-email"></use></svg></a>
+
+        <a href="https://api.whatsapp.com/send?phone=50581966620" target="_blank" title="Whatsapp" class="link-icon i-whatsapp"><svg class="icon"><use xlink:href="#icon-whatsapp"></use></svg></a>
+
+        <a href="https://facebook.com/hespinoza97" target="_blank" title="Facebook" class="link-icon i-facebook"><svg class="icon"><use xlink:href="#icon-facebook"></use></svg></a>
+
+      </div>
+      <footer class="building-site--footer">&#169; 2019. Hecho por Harold Espinoza</footer>
+</section>
+`;
